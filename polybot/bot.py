c993d6bef684bd4875bcf1f2906aa44a71d0e57f
shantalberkhof <shantal.berkhof@gmail.com>
2024-05-27 20:19:42 +0300

    Docker Project

--- polybot/bot.py
+++ polybot/bot.py
@@ -120,7 +120,7 @@ class ObjectDetectionBot(Bot):
             #json_data = response.json()
             # TODO send the returned results to the Telegram end-user
             # turn response.text into something normal - Not required.
-            # A change
+            # A change 2
             #json_data = response.json()
             #data = json_data
             #object_counts = {}
